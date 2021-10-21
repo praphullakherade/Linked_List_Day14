32ff720637def74fbc034d37dcef807b30a51edc
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-21 12:54:47 +0530

    Create Simple Linekd List

--- LinkedList.java
+++ LinkedList.java
@@ -2,7 +2,20 @@ package com.linkedlist;
 
 public class LinkedList {
     public static void main(String[] args) {
-        System.out.println("Welcome to Linked List Problem");
+
+        Node node1 = new Node(56);
+        Node node2 = new Node(70);
+        Node node3 = new Node(30);
+
+        Node head = node1;
+        node1.next = node2;
+        node2.next = node3;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
 
