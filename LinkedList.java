8cce20410911d1baa36d65c10d7302682e98d200
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-22 11:44:41 +0530

    Ordered linked list in ascending order

--- LinkedList.java
+++ LinkedList.java
@@ -2,21 +2,17 @@ package com.linkedlist;
 
 public class LinkedList {
     public static void main(String[] args) {
-            UC9 uc9 = new UC9();
-            uc9.push(56);
-            uc9.push(30);
-            uc9.push(70);
+            UC10 uc10 = new UC10();
+            uc10.push(56);
+            uc10.push(30);
+            uc10.push(70);
             System.out.println("Linked list");
-            uc9.print();
+            uc10.print();
 
-            uc9.insertAfter(40);
-            uc9.print();
+            uc10.sortList();
+            System.out.println("After sorting ");
+            uc10.print();
 
-            uc9.size();
-            uc9.deleteAfter(40);
-            System.out.println("After delete ");
-            uc9.print();
-            uc9.size();
     }
 }
 
---
+++ UC10.java
@@ -0,0 +1,55 @@
+package com.linkedlist;
+
+public class UC10 {
+    Node head;
+    Node tail;
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;
+            this.head = newNode;
+            newNode.next = temp;
+        }
+    }
+
+    public void sortList() {
+
+        Node current = head, index = null;
+        int temp;
+
+        if (head == null) {
+            return;
+        } else {
+            while (current != null) {
+                index = current.next;
+
+                while (index != null) {
+                    if (current.data > index.data) {
+                        temp = current.data;
+                        current.data = index.data;
+                        index.data = temp;
+                    }
+                    index = index.next;
+                }
+                current = current.next;
+            }
+        }
+    }
+
+    public void print() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+        }
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+
+    }
+}
